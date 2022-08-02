3a340ed63deeeca070bed61bd9afdcb06abcf8ea
matthijsvanderplas <mvdplas84@ziggo.nl>
2022-08-02 15:22:56 +0200

    Matchmaking functionality completed

--- matchmaking.js
+++ matchmaking.js
@@ -1,4 +1,4 @@
-function createMatchmakingCardToDom (parent, data, index) {
+function createMatchmakingCard (data, btn) {
     const el = document.createElement('div')
     el.classList.add('card')
     const img = document.createElement('img')
@@ -13,20 +13,27 @@ function createMatchmakingCardToDom (parent, data, index) {
     const country = document.createElement('p')
     country.innerText = `Country: ${data.region}`
     const zodiac = document.createElement('p')
-    zodiac.innerText = `Zodiac: ${getZodiac(Number(data.birthday.dmy.slice(0,2)), Number(data.birthday.dmy.slice(3,5)))}`
-    const match = document.createElement('div')
-    match.classList.add('card__name')
-    const button = document.createElement('button')
-    button.classList.add('right')
-    button.id = index;
-    button.innerHTML = `Find Matches!`
+    const zodiacSign = getZodiac(Number(data.birthday.dmy.slice(0,2)), Number(data.birthday.dmy.slice(3,5)))
+    zodiac.innerText = `Zodiac: ${zodiacSign}`
     text.appendChild(name)
     text.appendChild(age)
     text.appendChild(country)
     text.appendChild(zodiac)
     el.appendChild(img)
     el.appendChild(text)
-    el.appendChild(button)
+    if (btn){
+        const button = document.createElement('button')
+        button.classList.add('right')
+        button.innerHTML = `Find Matches!`
+        button.addEventListener('click', () => {
+            const person = getSinglePersonData(data.name, data.surname)
+            const matches = getMatches(zodiacSign)
+            createMatchesOverview(person, matches)
+
+        
+     })
+     el.appendChild(button)
+    }
     return el
 }
 
@@ -34,5 +41,57 @@ const matchmaking = document.querySelector('#matchmaking').addEventListener('cli
     e.preventDefault;
     clearScreen();
     const adultsOnly = getAdultList();
-    adultsOnly.forEach(adult => addToDom(createMatchmakingCardToDom(parent, adult)))
+    adultsOnly.forEach(adult => addToDom(createMatchmakingCard(adult, 'y')))
 });
+
+function createMatchesOverview(person, matches) {
+    clearScreen();
+    const subjectCard = createSubjectCard(person[0], matches)
+    addToDom(subjectCard)
+    const btn = document.createElement('button')
+    btn.innerText = `Reset`
+    btn.classList.add('card__span')
+    btn.style.marginBottom = '2rem'
+    btn.addEventListener('click', () => {
+        clearScreen();
+        const adultsOnly = getAdultList();
+        adultsOnly.forEach(adult => addToDom(createMatchmakingCard(adult, 'y')))
+    })
+    addToDom(btn)
+    getZodiacList(matches[0]).forEach(person => addToDom(createMatchmakingCard(person)))
+}
+
+function createSubjectCard(person, matches) {
+    const el = document.createElement('div')
+    el.classList.add('card', 'card__span')
+    const img = document.createElement('img')
+    img.classList.add('card__img')
+    img.src = person.photo;
+    const text = document.createElement('div')
+    text.classList.add('card__name')
+    const name = document.createElement('p')
+    name.innerText = `${person.name} ${person.surname}`
+    const age = document.createElement('p')
+    age.innerText = `Age: ${person.age}`
+    const country = document.createElement('p')
+    country.innerText = `Country: ${person.region}`
+    const zodiac = document.createElement('p')
+    const zodiacSign = getZodiac(Number(person.birthday.dmy.slice(0,2)), Number(person.birthday.dmy.slice(3,5)))
+    zodiac.innerText = `Zodiac: ${zodiacSign}`
+    const match = document.createElement('div')
+    match.classList.add('card__name')
+    const matchedZodiac = document.createElement('p')
+    matchedZodiac.innerText = `You match with: ${matches[0]}`
+    const zodiacText = document.createElement('p')
+    zodiacText.innerText = `${matches[1]}`
+    match.appendChild(matchedZodiac)
+    match.appendChild(zodiacText)
+    text.appendChild(name)
+    text.appendChild(age)
+    text.appendChild(country)
+    text.appendChild(zodiac)
+    el.appendChild(img)
+    el.appendChild(text)
+    el.appendChild(match)
+    return el
+}
